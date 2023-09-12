51ca2e2c328bd4eb2f5c8560ee9eeb591dd83336
Isa <isabelymarques844@gmail.com>
2023-09-12 19:32:54 -0300

    App2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,5 +7,16 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+  resultado:any;
 
+  SalLiq(formu:any){           
+    let SalB=parseFloat(formu.value.sb);
+    let ValH=parseFloat(formu.value.vl);
+    let Provento=SalB+ValH;
+
+    let DescF=parseFloat(formu.value.df);
+    let DescIR=parseFloat(formu.value.dc);
+    let Desconto=DescF+(DescIR/100*SalB);
+    this.resultado=Provento-Desconto;
+    }
 }
